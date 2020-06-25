ed896fec748554f5711b3b2fcaed042ff525acd8
朱骏杰 <ories.zhu@mochain.vip>
2020-06-25 18:16:53 +0800

    可读的流的暂停与恢复

--- 3.js
+++ 3.js
@@ -5,6 +5,19 @@ const server = http.createServer()
 server.on('request',(request, response)=>{
   const stream = fs.createReadStream('./big_file.txt')
   stream.pipe(response)
+  stream.pause()
+  setTimeout(()=>{
+    stream.resume()
+  },10000)
+
+  stream.on('data',(chunk)=>{
+    console.log(chunk.toString())
+    console.log('读取了一次数据')
+  })
+  stream.on('end',()=>{
+    console.log('全部读完了')
+  })
+  // stream.pipe(response)
 })
 
 server.listen(8888)
