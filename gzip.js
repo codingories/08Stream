f05ecbe34cf5d171f53cc7e4791868b742099ac3
朱骏杰 <ories.zhu@mochain.vip>
2020-06-26 10:32:50 +0800

    通过流文件加密

--- gzip.js
+++ gzip.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const zlib = require('zlib');
 const file = process.argv[2];
+const crypto = require("crypto")
 
 const { Transform } = require("stream");
 
@@ -13,6 +14,7 @@ const reportProgress = new Transform({
 
 
 fs.createReadStream(file)
+  .pipe(crypto.createCipher("aes192","123456")) // 一定要先加密再压缩
   .pipe(zlib.createGzip())
   // .on("data",()=>process.stdout.write(".")) // gzip的过程冲打印点,然后存入到后面
   .pipe(reportProgress) // 优化打点
