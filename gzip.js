92b75196b2734ccdd7f2f42a06008fda47419fe2
朱骏杰 <ories.zhu@mochain.vip>
2020-06-26 10:26:21 +0800

    优化打点

--- gzip.js
+++ gzip.js
@@ -2,6 +2,19 @@ const fs = require('fs');
 const zlib = require('zlib');
 const file = process.argv[2];
 
+const { Transform } = require("stream");
+
+const reportProgress = new Transform({
+  transform(chunk, encoding, callback){
+    process.stdout.write(".");
+    callback(null, chunk)
+  }
+});
+
+
 fs.createReadStream(file)
-.pipe(zlib.createGzip())
-.pipe(fs.createWriteStream(file+".gz"))
+  .pipe(zlib.createGzip())
+  // .on("data",()=>process.stdout.write(".")) // gzip的过程冲打印点,然后存入到后面
+  .pipe(reportProgress) // 优化打点
+  .pipe(fs.createWriteStream(file+".gz"))
+  .on("finish",()=>{console.log("done")})
